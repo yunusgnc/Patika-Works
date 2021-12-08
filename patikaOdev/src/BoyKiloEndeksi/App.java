c7b4bed24778b98aaf9dff4a0c9854bbbd665e74
yunusgnc <yunus0261@gmail.com>
2021-12-08 20:31:41 +0300

    Vücut Kitle İndeksi

---
+++ patikaOdev/src/BoyKiloEndeksi/App.java
@@ -0,0 +1,16 @@
+package BoyKiloEndeksi;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double boy, kilo, sonuc;
+        System.out.print("Boyunuzu Giriniz : ");
+        boy = inp.nextDouble();
+        System.out.print("Kilonuzu Giriniz :");
+        kilo = inp.nextDouble();
+        sonuc = kilo / Math.pow(boy, 2);
+        System.out.println("Vücut Kitle İndeksiniz :" + sonuc);
+    }
+}
