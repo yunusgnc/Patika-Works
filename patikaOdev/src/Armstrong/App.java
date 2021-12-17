c383a4a18dedb1326687cd4efaca33746e2932a9
yunusgnc <yunus0261@gmail.com>
2021-12-17 14:51:03 +0300

    uslu sayilar

---
+++ patikaOdev/src/Armstrong/App.java
@@ -0,0 +1,24 @@
+package Armstrong;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n, result = 0, r;
+        System.out.print("Sayıyı Giriniz :");
+        n = inp.nextInt();
+        r = n;
+        int counter = 0;
+        while (n != 0) {
+            n /= 10;
+            counter++;
+        }
+        for (int i = 0; i < counter; i++) {
+            n = r % 10;
+            r /= 10;
+            result += n;
+        }
+        System.out.println(result);
+    }
+}
