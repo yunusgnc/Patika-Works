bbd6d56b04839060a5378a306c7a86cb77aeb857
yunusgnc <yunus0261@gmail.com>
2021-12-18 23:07:00 +0300

    Ebob Ekok

---
+++ patikaOdev/src/EbobEkok/App.java
@@ -0,0 +1,34 @@
+package EbobEkok;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("1. Sayıyı Giriniz : ");
+        num1 = input.nextInt();
+        System.out.print("1. Sayıyı Giriniz : ");
+        num2 = input.nextInt();
+        int ebob = 0, ekok;
+
+        int i = num1;
+
+        while (i >= 1) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                ebob = i;
+                break;
+            }
+            i--;
+        }
+
+        System.out.println("Ebob : " + ebob);
+
+        ekok = (num1 * num2) / ebob;
+
+        System.out.println("Ekok : " + ekok);
+
+    }
+}
